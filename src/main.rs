bb953eeea16d2a6709282bfa61356ad4b15d50dd
Thomas Sheard <thomas.sheard26@gmail.com>
2024-08-27 21:01:05 +1000

    file input and reading contents

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+
+    println!("loading file {file_path}");
+
+    let contents = fs::read_to_string(file_path)
+        .expect("should have been able to read the file");
+
+    println!("which contains:\n{contents}");
+
 }
